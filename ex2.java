67bd55ee11a41ff616ec7fcdcf4dd1db3dfccc2b
Kunalkhalkar <Kunal6khalkar@gmail.com>
2024-02-17 22:40:06 +0530

    ex2 added

---
+++ ex2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ex2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter value of a");
+        int a=sc.nextInt();
+        System.out.println("enter your full name");
+        String str = sc.nextLine();
+    
+        System.out.println("hello "+str+" your value is "+a);
+
+    }
+}
