b188424ad0085210dc013bc66ffdde0b85a17115
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-04-10 22:28:30 +0530

    Add user input for variable 'a' in ex2.java

--- ex2.java
+++ ex2.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class ex2 {
     
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("enter value of a");
         int a=sc.nextInt();
