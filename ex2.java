a531a3a76e15a4a1ac94c360304c001d1f35b04d
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-04-30 22:50:03 +0530

    Correct input handling and output formatting
    Fix input reading for variable 'str' and adjust output message.

--- ex2.java
+++ ex2.java
@@ -6,10 +6,8 @@ public class ex2 {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter value of a");
         int a=sc.nextInt();
-        
         System.out.println("enter your full name");
         String str = sc.nextLine();
-        
         System.out.println("hello "+str+" your value is "+a);
 
     }
