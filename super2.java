946730d9de180c9c944795ef3cd78e2f6ad8f84c
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-04-30 22:50:37 +0530

    Update super2.java

--- super2.java
+++ super2.java
@@ -11,15 +11,12 @@ class tractor{
         System.out.println("this is tractor with param constructor : "+b);
     }
 }
- 
 class jcb{
        public jcb(int r){
                 System.out.println("this sound is with parameter constructor : "+r);
        } 
 }
-
 class hevy_machine extends tractor{
-
  public hevy_machine(int r ,int c){
     super(r);
     System.out.println("hi i am the heavy machine : "+ c);
@@ -29,6 +26,5 @@ public class super2 {
     public static void main(String[] args) {
        // tractor obj =new tractor();
         hevy_machine obj1 =new hevy_machine(10, 5);
-
     }
 }
