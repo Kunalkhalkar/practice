03460c03ab6cb664a05c15231f316afabde31220
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-06-19 23:06:27 +0530

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -1,6 +1,5 @@
 import java.util.*;
-public class ex2 {
-    
+public class ex2 {    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter value of a");
@@ -8,6 +7,5 @@ public class ex2 {
         System.out.println("enter your full name");
         String str = sc.nextLine();
         System.out.println("hello "+str+" your value is "+a);
-
     }
 }
